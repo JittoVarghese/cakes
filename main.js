2edb057c7081236406777fa53fda91dca789bccb
JittoVarghese <119120361+JittoVarghese@users.noreply.github.com>
2024-10-20 19:35:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Text animation starts
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+    entry.target.classList.add('reveal');
+    } else {
+    entry.target.classList.remove('reveal');
+    }
+     });
+
+    });
+    const hiddenElements = document.querySelectorAll('.hide');
+    hiddenElements.forEach((el) => observer.observe(el));
+    
+// Text animation ends
